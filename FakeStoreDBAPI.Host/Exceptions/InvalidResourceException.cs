20f4c47799d6552af22f18d9ed02fd6b3761fd18
Lucca Tirelli <tirellilucca@gmail.com>
2025-12-09 10:49:40 -0300

    Add summary for InvalidResourceException

--- FakeStoreDBAPI.Host/Exceptions/InvalidResourceException.cs
+++ FakeStoreDBAPI.Host/Exceptions/InvalidResourceException.cs
@@ -1,5 +1,8 @@
 ﻿namespace FakeStoreDBAPI.Host.Exceptions
 {
+    /// <summary>
+    /// Exception used to indicate that a resource used in the request body is not valid
+    /// </summary>
     public class InvalidResourceException : Exception
     {
         public InvalidResourceException(string message) : base(message)
