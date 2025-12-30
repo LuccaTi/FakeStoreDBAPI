ace09098fc9d720e6850b5dbfec10e32ffaa23fa
Lucca Tirelli <tirellilucca@gmail.com>
2025-12-30 12:10:38 -0300

    refactor: Add Customer date create to GET DTO

--- FakeStoreDBAPI.Host/DTO/Customer/CustomerDto.cs
+++ FakeStoreDBAPI.Host/DTO/Customer/CustomerDto.cs
@@ -9,5 +9,6 @@
         public string? LastName { get; set; }
         public string? Phone { get; set; }
         public bool IsActive { get; set; }
+        public DateTime DateCreate { get; set; }
     }
 }
